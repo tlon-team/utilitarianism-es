07a915d4fa58fb38273bd8f70fc4ec4c14e9e761
Pablo Stafforini <pablo@stafforini.com>
2023-08-18 22:06:19 +0200

    Add .dir-locals.el

---
+++ translations/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+((markdown-mode . ((jinx-languages . "es")
+		   (jinx-local-words . "Does Is Objections act and authors avorecer chap ctuar description draft false generalesl gradientBottom gradientTop how immorally means menu next objection page permissive tell title to type ualquiera us utilitarianism weight"))))
