bbd44a7f4b62556cbfdd8d89fd501e9bc67425d6
Pablo Stafforini <pablo@stafforini.com>
2023-08-31 17:27:59 +0200

    Update .dir-locals.el

--- translations/.dir-locals.el
+++ translations/.dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 ((markdown-mode . ((jinx-languages . "es")
-		   (jinx-local-words . "Does Is Objections act and authors avorecer chap ctuar description draft false generalesl gradientBottom gradientTop how immorally means menu next objection page permissive tell title to type ualquiera us utilitarianism weight"))))
+		   (jinx-local-words . "Does Is Objections act and authors avorecer chap ctuar description draft false generalesl gradientBottom gradientTop how immorally means menu next objection page permissive tell title to type ualquiera us utilitarianism weight")
+		   (eval . (add-hook 'after-save-hook #'tlon-babel-check-file-title-match nil t))
+		   (eval . (add-hook 'after-save-hook #'tlon-babel-fix-curly-quotes nil t)))))
