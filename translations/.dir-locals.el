9ca7e2313b10fd4b1682f9127b3dfc075b851309
Pablo Stafforini <pablo@stafforini.com>
2023-09-20 13:05:06 +0200

    Update .dir-locals.el

--- translations/.dir-locals.el
+++ translations/.dir-locals.el
@@ -3,4 +3,5 @@
 ((markdown-mode . ((jinx-languages . "es")
 		   (jinx-local-words . "Does Is Objections act and authors avorecer chap ctuar description draft false generalesl gradientBottom gradientTop how immorally means menu next objection page permissive tell title to type ualquiera us utilitarianism weight")
 		   (eval . (add-hook 'after-save-hook #'tlon-babel-check-file-title-match nil t))
-		   (eval . (add-hook 'after-save-hook #'tlon-babel-fix-curly-quotes nil t)))))
+		   (eval . (add-hook 'after-save-hook #'tlon-babel-fix-curly-quotes nil t))
+		   (eval . (add-hook 'after-save-hook #'tlon-babel-fix-footnote-punctuation nil t)))))
